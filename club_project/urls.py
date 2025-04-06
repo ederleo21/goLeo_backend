009192eb9f5dd77b228ea1623f9cf333e5ca9d84
ederleo <ederordonez10@gmail.com>
2025-04-06 15:58:29 -0500

    v

--- club_project/urls.py
+++ club_project/urls.py
@@ -18,6 +18,6 @@ urlpatterns = [
     path("send-email/", send_contact_email, name="send_email")
 ]
 
-if settings.DEBUG:
+if not settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
                                 
