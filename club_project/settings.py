3ea3106a319fe1d4c224b0724eef60dc848e6a22
ederleo <ederordonez10@gmail.com>
2025-04-06 20:59:53 -0500

    listo

--- club_project/settings.py
+++ club_project/settings.py
@@ -174,8 +174,8 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 #media
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# MEDIA_URL = '/media/'
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
